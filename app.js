5fdbb495531d535bfeb302084373df58fef428e7
leopoldkwok <leopoldkwok@gmail.com>
2023-11-02 10:49:13 +0000

    Basic Routes

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-
+require('express-async-errors');
 //express
 
 const express = require('express');
@@ -7,6 +7,19 @@ const app = express();
 
 // database
 const connectDB = require('./db/connect');
+
+// middleware
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('e-commerce api');
+});
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5001;
 const start = async () => {
