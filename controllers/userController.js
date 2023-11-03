b327ebeccd7aa45489d53f3804ebcb75af17b944
leopoldkwok <leopoldkwok@gmail.com>
2023-11-03 16:01:53 +0000

    CheckPermissions Function

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,11 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
-const { createTokenUser, attachCookiesToResponse } = require('../utils');
+const {
+  createTokenUser,
+  attachCookiesToResponse,
+  checkPermissions,
+} = require('../utils');
 
 const getAllUsers = async (req, res) => {
   console.log(req.user);
@@ -14,6 +18,8 @@ const getSingleUser = async (req, res) => {
   if (!user) {
     throw new CustomError.NotFoundError(`No user with id : ${req.params.id}`);
   }
+
+  checkPermissions(req.user, user._id);
 
   res.status(StatusCodes.OK).json({ user });
 };
