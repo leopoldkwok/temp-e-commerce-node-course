20c450ad702b4507c9e98a250cf7e9502c3deb20
leopoldkwok <leopoldkwok@gmail.com>
2023-11-07 13:07:51 +0000

    Order Structure

---
+++ controllers/orderController.js
@@ -0,0 +1,24 @@
+const createOrder = async (req, res) => {
+  res.send('create order');
+};
+
+const getAllOrders = async (req, res) => {
+  res.send('get all order');
+};
+const getSingleOrder = async (req, res) => {
+  res.send('get single order');
+};
+const getCurrentUserOrders = async (req, res) => {
+  res.send('get current user order');
+};
+const updateOrder = async (req, res) => {
+  res.send('update order');
+};
+
+module.exports = {
+  getAllOrders,
+  getSingleOrder,
+  getCurrentUserOrders,
+  createOrder,
+  updateOrder,
+};
